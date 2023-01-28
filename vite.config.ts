877ee08010f51ca96bffbbe0eef8d9f6be2113ad
MathisHumbert <mathishumbert71@gmail.com>
2023-01-28 18:38:10 +0100

    trying to fix error on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rewrites: [{ source: '/(.*)', destination: '/' }],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'pages/Home.tsx'),
+        about: resolve(root, 'pages/About.tsx'),
+      },
+    },
+  },
 });
