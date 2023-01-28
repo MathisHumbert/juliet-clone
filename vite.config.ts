6c34fdbe35bbb110ece079b3443ec3b955682ed8
MathisHumbert <mathishumbert71@gmail.com>
2023-01-28 18:13:46 +0100

    trying to fix error on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import reactRouter from 'vite-plugin-react-router';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/juliet-clone.vercel.app/',
+  plugins: [react(), reactRouter()],
 });
